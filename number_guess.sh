41baee68d1a0078ed008a99d1b3b8a370b665f39
Mark Gomez <gomezmark090@gmail.com>
2025-02-23 14:48:25 +0000

    fix: correct the username greeting for returning users

--- number_guess.sh
+++ number_guess.sh
@@ -2,11 +2,10 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# feat: Add the initial number guessing game logic with user login and database tracking
+# fix: Correct the greeting for returning users, including the number of games and best game
 echo -e "\nEnter your username:"
 read USERNAME
 
-# Check if the username is empty
 if [[ -z $USERNAME ]]; then
   echo "Username cannot be empty. Exiting..."
   exit 1
@@ -15,41 +14,17 @@ fi
 USER_ID=$($PSQL "SELECT user_id FROM public.players WHERE username='$USERNAME'")
 
 if [[ -z $USER_ID ]]; then
-  # User doesn't exist, create them
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO public.players(username) VALUES ('$USERNAME')")
   USER_ID=$($PSQL "SELECT user_id FROM public.players WHERE username='$USERNAME'")
 else
-  # User exists, get game stats
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM public.games WHERE user_id=$USER_ID")
   BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM public.games WHERE user_id=$USER_ID")
-  
-  # If no best game yet, show N/A
+
   if [[ -z $BEST_GAME || $BEST_GAME == "NULL" ]]; then
     BEST_GAME="N/A"
   fi
 
+  # Fix: Properly greet users with the correct number of games played and best game
   echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
-
-SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
-GUESS_COUNT=1
-
-echo -e "\nGuess the secret number between 1 and 1000:"
-read USER_GUESS
-
-until [[ $USER_GUESS -eq $SECRET_NUMBER ]]; do
-  if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]; then
-    echo -e "\nThat is not an integer, guess again:"
-  elif [[ $USER_GUESS -lt $SECRET_NUMBER ]]; then
-    echo "It's higher than that, guess again:"
-  else
-    echo "It's lower than that, guess again:"
-  fi
-  read USER_GUESS
-  ((GUESS_COUNT++))
-done
-
-INSERT_GAME_RESULT=$($PSQL "INSERT INTO public.games(user_id, secret_number, number_of_guesses) VALUES ($USER_ID, $SECRET_NUMBER, $GUESS_COUNT)")
-
-echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
