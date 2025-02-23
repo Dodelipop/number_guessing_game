91073689fcfd8f0dee8819607f2d9555dfcd583a
Mark Gomez <gomezmark090@gmail.com>
2025-02-23 14:47:59 +0000

    feat: add number guess game with user login and database tracking

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# Ask for the username
+# feat: Add the initial number guessing game logic with user login and database tracking
 echo -e "\nEnter your username:"
 read USERNAME
 
@@ -12,22 +12,19 @@ if [[ -z $USERNAME ]]; then
   exit 1
 fi
 
-# Query the database to get user_id for the entered username
 USER_ID=$($PSQL "SELECT user_id FROM public.players WHERE username='$USERNAME'")
 
-# Check if the user already exists in the database
 if [[ -z $USER_ID ]]; then
-  # User does not exist, create a new user and greet them
+  # User doesn't exist, create them
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-  # Insert the new user into the database
   INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO public.players(username) VALUES ('$USERNAME')")
   USER_ID=$($PSQL "SELECT user_id FROM public.players WHERE username='$USERNAME'")
 else
-  # User exists, get the total number of games played and the best game score
+  # User exists, get game stats
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM public.games WHERE user_id=$USER_ID")
   BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM public.games WHERE user_id=$USER_ID")
-
-  # If there's no best game (i.e., no games played yet), set to "N/A"
+  
+  # If no best game yet, show N/A
   if [[ -z $BEST_GAME || $BEST_GAME == "NULL" ]]; then
     BEST_GAME="N/A"
   fi
@@ -35,15 +32,12 @@ else
   echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# Generate a random secret number between 1 and 1000
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
-GUESS_COUNT=1  # Initialize the guess count
+GUESS_COUNT=1
 
-# Start the game loop
 echo -e "\nGuess the secret number between 1 and 1000:"
 read USER_GUESS
 
-# Loop until the user guesses the correct number
 until [[ $USER_GUESS -eq $SECRET_NUMBER ]]; do
   if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]; then
     echo -e "\nThat is not an integer, guess again:"
@@ -56,16 +50,6 @@ until [[ $USER_GUESS -eq $SECRET_NUMBER ]]; do
   ((GUESS_COUNT++))
 done
 
-# After guessing correctly, insert the game results into the database
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO public.games(user_id, secret_number, number_of_guesses) VALUES ($USER_ID, $SECRET_NUMBER, $GUESS_COUNT)")
 
-
 echo -e "\nYou guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
-
-
-
-
-
-
-
-
